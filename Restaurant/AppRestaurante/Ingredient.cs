448a169d46ecb22cb495e8a05fcad5b51d8babae
Rover Serrano <73447289+Rovercito@users.noreply.github.com>
2022-02-22 23:15:16 -0400

    Practica no concluida

---
+++ Restaurant/AppRestaurante/Ingredient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRestaurante
+{
+    class Ingredient
+    {
+        public string Category { get; set; }
+
+        List<Menu> MenuList;
+
+        public List<Menu> Menus { get { return MenuList; } }
+
+        public Ingredient(string category)
+        {
+            Category = category;
+            MenuList = new List<Menu>();
+        }
+
+        public Ingredient(string category, List<Menu> menus)
+        {
+            Category = category;
+            MenuList = new List<Menu>();
+        }
+
+        public void AddMenu(List<Menu> newList)
+        {
+            MenuList = newList;
+        }
+    }
+}
---
+++ Restaurant/AppRestaurante/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRestaurante
+{
+     class Menu
+    {
+        public string MenuName { get; set; }
+        public string Ingredient { get; set; }
+
+
+        public Menu(string name, string ingredient)
+        {
+            MenuName = name;
+            Ingredient = ingredient;
+        }
+    }
+}
